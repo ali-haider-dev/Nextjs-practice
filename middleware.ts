2657a5dcb66518291dd71235b87b11de2c78f888
alihaider-work <a.haider@kuduconsulting.com>
2025-09-08 14:01:40 +0500

    conditional redirect in middlewere

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/api/time", request.url));
+  const response = NextResponse.next();
+  const token = request.cookies.get("theme")?.value;
+  console.log("Middleware Token:", token);
+  if (request.nextUrl.pathname === "/api/profile" && !token) {
+    return NextResponse.redirect(new URL("/api/time", request.url));
+  }
+  return response;
 }
-
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/api/profile",};
+// export const config = {
+//   matcher: "/api/profile",};
