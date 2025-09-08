6271d7f164ffb538c0c78850947f2c84c4fadfb5
alihaider-work <a.haider@kuduconsulting.com>
2025-09-08 12:04:43 +0500

    cookies in route handlers

--- app/profile/route.ts
+++ app/profile/route.ts
@@ -1,5 +1,16 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { comments } from "./data";
-export const GET = async (request: Request) => {
-  return new Response("<h1>Profile API Data </h1>",{headers: { 'Content-Type': 'text/html' }});
+import { NextRequest } from "next/server";
+export const GET = async (request: NextRequest) => {
+  const headersList = await headers();
+  console.log("headersList", headersList.get("user-agent"));
+  const token = request.cookies.get("token")?.value;
+  const name = request.cookies.get("name")?.value;
+  console.log("token", token);
+  console.log("name", name);
+  const cookieStore = await cookies();
+  cookieStore.set("name", "Ali");
+  return new Response("<h1>Profile API Data </h1>", {
+    headers: { "Content-Type": "text/html", "Set-Cookie": "token=Ali123" },
+  });
 };
